c71ac66e6f55c13b4f283da7cfcded9d2c46bbeb
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-19 10:08:19 +0530

    Create pr10.c

---
+++ As15 sol/pr10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[10]={5,4,6,7,7,8,9,8,9,10};
+     int freq[20]={0};
+    for(i=0;i<10;i++)
+    {
+        freq[a[i]]++;
+    }
+    for(i=0;i<20;i++)
+    {
+        if(freq[i]!=0)
+            printf("%d==>%d\n",i,freq[i]);
+    }
+}
