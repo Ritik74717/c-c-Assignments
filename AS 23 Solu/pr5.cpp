ed34123f56308e9f1dfaf9598b56e7457b844b00
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-21 18:12:15 +0530

    Create pr5.cpp

---
+++ AS 23 Solu/pr5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,w,h,v;
+    cout<<"enter length,width and height";
+    cin>>l>>w>>h;
+    v=l*w*h;
+    cout<<" Volume is "<<v;
+    return 0;
+}
