e5363eaa7be2b5bb1494628244d281969017e981
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-21 18:36:24 +0530

    Create pr10.cpp

---
+++ AS 23 Solu/pr10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a[10];
+   int i,sum=0;
+   cout<<"Enter 10 elements"<<endl;
+   for(i=0;i<10;i++)
+   {
+       cin>>a[i];
+   }
+   for(i=0;i<10;i++)
+   {
+       sum=sum+a[i];
+   }
+   cout<<"Sum of elements is "<<sum;
+}
