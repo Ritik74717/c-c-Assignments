c4f44360668f5db6d2cca9618d59262c006d4432
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-21 18:18:51 +0530

    Create pr6.cpp

---
+++ AS 23 Solu/pr6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    float avg;
+    cout<<"enter three numbers";
+    cin>>x>>y>>z;
+    avg=(float)(x+y+z)/3;
+    cout<<" Average is "<<avg;
+    return 0;
+}
