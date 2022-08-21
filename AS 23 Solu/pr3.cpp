a63a2aff6ba2da8191fd8d7681cff0f3c09f9742
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-21 18:02:00 +0530

    Create pr3.cpp

---
+++ AS 23 Solu/pr3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y,z;
+  cout<<"Enter Two Numbers";
+  cin>>x>>y;
+  z=x+y;
+  cout<<" Sum of "<<x<<" and "<<y<<" is "<<z;
+
+    return 0;
+}
