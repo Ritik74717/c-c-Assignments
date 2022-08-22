71afc8d7559d0955dff30028cc03b8127897140d
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-22 09:47:35 +0530

    Create pr2.c

---
+++ AS 22 SOL/pr2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   int *p,x,sum=0;
+   float avg;
+   printf("enter array size\n");
+   scanf("%d",&x);
+   printf("Enter %d values\n",x);
+   p=(int*)calloc(x,sizeof(int));
+   int i;
+   for(i=0;i<x;i++)
+   {
+       scanf("%d",p+i);
+   }
+   printf("\n");
+   printf("Entered values\n");
+   for(i=0;i<x;i++)
+   {
+       printf("%d ",*(p+i));
+       sum =sum + (*(p+i));
+   }
+   printf("\n");
+   avg=(float)sum/x;
+   printf("Average = %f\n",avg);
+   return 0;
+
+
+
+}
