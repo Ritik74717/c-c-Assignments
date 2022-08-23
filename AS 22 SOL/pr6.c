704d74672dc193f9ce55e0448242b3331f8623f9
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-08-23 09:02:27 +0530

    Create pr6.c

---
+++ AS 22 SOL/pr6.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+  int *p;
+  int i,n,max=0;
+  printf("enter number of elements\n");
+  scanf("%d",&n);
+  p=(int*)malloc(sizeof(int)*n);
+  printf("enter %d values\n",n);
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",(p+i));
+  }
+  printf("entered values are\n");
+  for(i=0;i<n;i++)
+  {
+      printf("%d ",*(p+i));
+  }
+  printf("\n");
+  for(i=0;i<n;i++)
+  {
+      if(*(p+i) >= max)
+        max = (*(p+i));
+
+
+  }
+  printf("max=%d",max);
+
+
+
+
+   return 0;
+
+
+
+}
