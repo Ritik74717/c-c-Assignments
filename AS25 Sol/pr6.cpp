271cd64ae68bdac52e4a42a5b94d4c35b0a566be
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-09-06 16:30:39 +0530

    Create pr6.cpp

---
+++ AS25 Sol/pr6.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class square
+{ 
+     private:
+           int n;
+     public:
+           void set(int x)
+           {
+               n=x*x;
+           }
+           int get()
+           {
+
+               return n;
+           }
+
+};
+int main()
+{
+   square s1;
+   s1.set(5);
+   cout<<"Square is "<<s1.get()<<endl;
+
+}
