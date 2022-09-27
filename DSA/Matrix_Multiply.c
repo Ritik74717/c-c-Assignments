c0c3c191a46df5ee4e3c32908e353e631f117b47
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-09-27 20:55:08 +0530

    Create Matrix_Multiply.c

---
+++ DSA/Matrix_Multiply.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3],c[3][3],sum=0,k,i,j;
+    printf("Enter 9 elements of first matrix\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Enter 9 elements of second matrix\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            sum=0;
+            for(k=0;k<3;k++)
+            {
+                sum=sum+a[i][k]*b[k][j];
+                c[i][j]=sum;
+            }
+
+        }
+    }
+    printf("First matrix is\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+     printf("Second matrix is\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+
+     printf(" Matrix Multiplication is\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
