0a215ecbc691cb8d0925047b21eec907aa9481eb
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-11-01 23:30:47 +0530

    Create pr3.cpp

---
+++ AS 36 SOL/pr3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include<array>
+#include<tuple>
+int main()
+{
+    array<int,5> ar;
+    int i;
+    for(i=0;i<ar.size();i++)
+        ar[i]=i;
+    for(i=0;i<ar.size();i++)
+        cout<<ar[i]<<" ";
+    cout<<endl;
+    cout<<"total elements = "<<ar.size()<<endl;
+    cout<<"first element = "<<ar.front()<<endl;
+    cout<<"last element = "<<ar.back()<<endl;
+    return 0;
+
+}
