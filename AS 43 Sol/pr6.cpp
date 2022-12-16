4fd733a5d0badb3f905dfe31b4df4f93ba47a30e
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-12-16 10:57:57 +0530

    Create pr6.cpp

---
+++ AS 43 Sol/pr6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    priority_queue<int,vector<int>,greater<int>> q1;
+    q1.push(12);
+    q1.push(11);
+    q1.push(34);
+    q1.push(2);
+    q1.push(7);
+    while(!q1.empty())
+    {
+        cout<<q1.top()<<" ";
+        q1.pop();
+    }
+    return 0;
+}
