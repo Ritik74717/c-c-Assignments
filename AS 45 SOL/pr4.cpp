236116021c759eb716ce7441f3b858ed8cce1364
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-12-29 14:26:15 +0530

    Create pr4.cpp

---
+++ AS 45 SOL/pr4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unordered_multiset<int> m;
+    unsigned int max_Size;
+    max_Size=m.max_size();
+    cout<<"Maximum Size = "<<max_Size;
+    return 0;
+
+}
