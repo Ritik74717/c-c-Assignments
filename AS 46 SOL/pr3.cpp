6bd52ae8773b77d96429a276a946455618922452
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-12-30 16:15:07 +0530

    Create pr3.cpp

---
+++ AS 46 SOL/pr3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   map<int,string> m1;
+   m1.insert({1,"Ritik Kaushik"});
+   m1.insert({2,"Adnan"});
+   m1.insert({3,"Satyam"});
+   m1.insert({4,"Parag"});
+
+   for(auto itr=m1.rbegin();itr!=m1.rend();itr++)
+    cout<<itr->first<<" "<<itr->second<<endl;
+
+    return 0;
+
+}
