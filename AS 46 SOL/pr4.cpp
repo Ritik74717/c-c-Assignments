3cf2159f1f51d7fb0520546f030e5ded0a8fc6ab
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2022-12-30 16:20:44 +0530

    Create pr4.cpp

---
+++ AS 46 SOL/pr4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   map<int,string> m1;
+   m1.insert({1,"Ritik Kaushik"});
+   m1.insert({2,"Adnan"});
+   m1.insert({3,"Satyam"});
+   m1.insert({4,"Parag"});
+
+   for(auto itr=m1.begin();itr!=m1.end();itr++)
+    cout<<itr->first<<" "<<itr->second<<endl;
+
+   cout<<"\nAfter Replacing\n";
+   m1[2]="HRK";
+   for(auto itr=m1.begin();itr!=m1.end();itr++)
+    cout<<itr->first<<" "<<itr->second<<endl;
+
+    return 0;
+
+}
